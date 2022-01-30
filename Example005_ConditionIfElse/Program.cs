d65d106a55c9423bf81f7a94f8db26063626b6fc
Odinokiy7 <koazo-maga@mail.ru>
2022-01-30 18:18:08 +0300

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "мама")
+{
+    Console.WriteLine("Ура, это же МАМА!");
+}
+else
+{
+    Console.Write("Ас саламу алейкум, ");
+    Console.WriteLine(username);
+}
