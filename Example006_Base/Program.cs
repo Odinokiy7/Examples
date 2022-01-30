d8797cc37205ab0047a5ce005c64842ea36bb68d
Odinokiy7 <koazo-maga@mail.ru>
2022-01-30 18:33:04 +0300

    Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 19;
+int b = 64;
+int c = 36;
+int d = 78;
+int e = 51;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
